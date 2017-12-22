58368b1979d144567c1f5ba548571ea499a2f364
Dejana Ćulibrk <34311604+Dejana1202@users.noreply.github.com>
2017-12-22 21:58:30 +0100

    Add files via upload
    Funkcija za pregled svih podataka za određenog kupca

---
+++ analyst.h
@@ -0,0 +1,18 @@
+#ifndef ANALITCOMMANDS_H
+#define ANALITCOMMANDS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "admin.h"
+#include "readfiles.h"
+#include "strukture.h"
+
+void analystMenu(NODE* );
+int provera_slova_analyst(char );  // za Analyst menu
+int provera_slovaYN_analyst(char ); // provjera yes || no za Analyst menu
+char ponovo_meni_analyst();
+void helpMenu_analyst(); // kratak opis funkcija u Analyst meniju
+void customerList(NODE *);
+void customerArtikls (NODE*,char*,char*);
+#endif // ANALITCOMMANDS_H
