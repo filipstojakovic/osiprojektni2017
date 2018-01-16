aba8243d09a5caebad7a7fc2f902cc5f7cc32884
Bilja <Bilja@Bilja-PC>
2018-01-16 23:54:11 +0100

    Prepravljanje formata int u float i adekvatno formatiranje programa

--- README.md
+++
@@ -1,2 +0,0 @@
-# osiprojektni2017
-Analiza troskova OSI 2017 grupa 17
---
+++ readformats.h
@@ -0,0 +1,16 @@
+#ifndef FORMAT1_H
+#define FORMAT1_H
+
+#include "strukture.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+
+POD readFormat1(char*);
+POD readFormat2(char*);
+POD readFormat3(char*);
+POD readFormat4(char*);
+POD readFormat5(char*);
+
+#endif // FORMAT1_H
